5113507678a6b9f0b23c4e03f2d49fa7c0d533fb
Your Name <viniciuscsaboia@gmail.com>
2024-04-19 11:30:24 -0300

    é

--- src/application/use-cases/cancel-notifications.spec.ts
+++ src/application/use-cases/cancel-notifications.spec.ts
@@ -2,9 +2,10 @@ import { Notification } from "@application/entities/notifications";
 import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repository";
 import { CancelNotification } from "./cancel-notification";
 import { Content } from "@application/entities/content";
+import { NotificationNotFound } from "./errors/notifications-not-found";
 
 describe('Cancel notification', () => {
-    it('should be able to send a notification', async () => {
+    it('should be able to cancel a notification', async () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
         const cancelNotification = new CancelNotification(notificationsRepository);
 
@@ -25,4 +26,16 @@ describe('Cancel notification', () => {
 
         expect(notificationsRepository.notifications[0].canceledAt).toEqual(expect.any(Date));
     });
+
+    it('should not be able  to cancel a non existing notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+        expect(() => {
+            return cancelNotification.execute({
+                notificationId: 'fake-notification-id'
+            })
+        }).rejects.toThrow(NotificationNotFound)
+
+    })
 });
